d8064462ee6685a007181921543eaa44fc52ecd9
benquach16 <benquach16@yahoo.com>
2014-01-16 17:50:47 -0800

    minor update

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
 #include <pthread>
 
+#include "client.h"
+#include "server.h"
 
 using namespace std;
 
 
 int main()
 {
-    //create sockets
+    //couple of things we have to do here
+    //namely, make a CLIENT and SERVER
+    //one person will be the client, and the other the server
     return 0;
 }
